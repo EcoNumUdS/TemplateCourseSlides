9bc3a89b0325dcce88fa24d2a98c90683ca92008
SteveViss <s.vissault@yahoo.fr>
2016-10-20 17:39:01 -0400

    Issue with path on ./script

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf ../pres/.cache
-cat *.Rmd > ../pres/index.Rmd
-Rscript --no-site-file --no-init-file ../scripts/get_slidify.R
+rm -rf ./pres/.cache
+cat *.Rmd > ./pres/index.Rmd
+Rscript --no-site-file --no-init-file ./scripts/get_slidify.R
