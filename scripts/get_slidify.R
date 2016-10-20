f0883d0b52a9c6ca0cd73fdf98c89bc9721fc4d8
SteveViss <s.vissault@yahoo.fr>
2016-10-20 17:48:53 -0400

    Logical test to install dependancies

--- scripts/get_slidify.R
+++ scripts/get_slidify.R
@@ -1,11 +1,14 @@
-### Installation package slidify if not installed
-# if (!require(slidify)){
-#   if (!require(devtools)) install.packages("devtools")
-#   else {
-#     install_github("ramnathv/slidify")
-#     install_github("ramnathv/slidifyLibraries")
-#   }
-# }
-### Load package slidify
-library(slidify)
-slidify('./pres/index.Rmd')
+if(Sys.getenv('TRAVIS') != true){
+  ## Installation package slidify if not installed
+  if (!require(slidify)){
+    if (!require(devtools)) install.packages("devtools")
+    else {
+      install_github("ramnathv/slidify")
+      install_github("ramnathv/slidifyLibraries")
+    }
+  }
+  ## Load package slidify
+  library(slidify)
+}
+
+slidify::slidify('./pres/index.Rmd')
