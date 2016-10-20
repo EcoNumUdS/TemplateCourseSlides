0c5b84fa600ab294779790f850a4e899f4b80dda
SteveViss <s.vissault@yahoo.fr>
2016-10-20 17:28:27 -0400

    fixing issue with dependancies

--- scripts/get_slidify.R
+++ scripts/get_slidify.R
@@ -1,11 +1,11 @@
 ### Installation package slidify if not installed
-if (!require(slidify)){
-  if (!require(devtools)) install.packages("devtools")
-  else {
-    install_github("ramnathv/slidify")
-    install_github("ramnathv/slidifyLibraries")
-  }
-}
+# if (!require(slidify)){
+#   if (!require(devtools)) install.packages("devtools")
+#   else {
+#     install_github("ramnathv/slidify")
+#     install_github("ramnathv/slidifyLibraries")
+#   }
+# }
 ### Load package slidify
 library(slidify)
 slidify('./pres/index.Rmd')
